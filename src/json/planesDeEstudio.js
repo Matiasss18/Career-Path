670cb85c4fb7733aa9ab51b2dbbaa06f29ba6a12
Matiasss18 <matiasmuraglia4@gmail.com>
2026-04-03 23:12:14 -0300

    plan completo naval

--- src/json/planesDeEstudio.js
+++ src/json/planesDeEstudio.js
@@ -246,23 +246,276 @@ const planes = {
     "nombre": "Ingeniería Naval",
     "area": "Naval",
     "universidad": "Universidad de Tecnológica Nacional",
-    "duracion": 5,
+    "duracion": 6,
     "materias": [
+      {
+        "nombre": "Introducción a la Ingeniería Naval",
+        "nivel": 1,
+        "código": "IIN"
+      },
       {
         "nombre": "Análisis Matemático I",
         "nivel": 1,
         "código": "AM1"
       },
       {
-        "nombre": "Sistemas y Procesos de Negocios",
+        "nombre": "Física I",
         "nivel": 1,
-        "código": "SyPN"
+        "código": "F1"
       },
       {
-        "nombre": "Lógica y Estructuras Discretas",
+        "nombre": "Inglés I",
         "nivel": 1,
-        "código": "LyED"
-      }
+        "código": "IT1"
+      },
+      {
+        "nombre": "Sistemas de Representación",
+        "nivel": 1,
+        "código": "SSRR"
+      },
+      {
+        "nombre": "Química General",
+        "nivel": 1,
+        "código": "QG"
+      },
+      {
+        "nombre": "Fundamentos de Informática",
+        "nivel": 1,
+        "código": "FI"
+      },
+      {
+        "nombre": "Dibujo Naval",
+        "nivel": 2,
+        "código": "DN",
+        "regularizadas": ["IIN", "SSRR"]
+      },
+      {
+        "nombre": "Física II",
+        "nivel": 2,
+        "código": "F2",
+        "regularizadas": ["AM1", "AGA", "F1"]
+      },
+      {
+        "nombre": "Análisis Matemático II",
+        "nivel": 2,
+        "código": "AM2",
+        "regularizadas": ["AM1", "AGA"]
+      },
+      {
+        "nombre": "Ingeniería y Sociedad",
+        "nivel": 2,
+        "código": "IngSoc"
+      },
+      {
+        "nombre": "Probabilidad y Estadística",
+        "nivel": 2,
+        "código": "PyE",
+        "regularizadas": ["AM1", "AGA"]
+      },
+      {
+        "nombre": "Física III",
+        "nivel": 2,
+        "código": "F3",
+        "regularizadas": ["AM1", "F1"]
+      },
+      {
+        "nombre": "Análisis Estructural I",
+        "nivel": 2,
+        "código": "AE1",
+        "regularizadas": ["AM1", "F1", "AGA"]
+      },
+      {
+        "nombre": "Legislación",
+        "nivel": 2,
+        "código": "Leg"
+      },
+      {
+        "nombre": "Teoría del Buque I",
+        "nivel": 3,
+        "código": "TB1",
+        "regularizadas": ["AM2", "PyE", "DN"],
+        "aprobadas": ["AM1", "IIN"]
+      },
+      {
+        "nombre": "Termodinámica",
+        "nivel": 3,
+        "código": "Term",
+        "regularizadas": ["AM2", "F2"]
+      },
+      {
+        "nombre": "Mecánica Racional",
+        "nivel": 3,
+        "código": "MR",
+        "regularizadas": ["AM2"],
+        "aprobadas": ["F1"]
+      },
+      {
+        "nombre": "Electrotecnia y Máquinas Eléctricas",
+        "nivel": 3,
+        "código": "EME",
+        "regularizadas": ["AM2"],
+        "aprobadas": ["AGA", "AM1"]
+      },
+      {
+        "nombre": "Matemática Aplicada a la Ingeniería",
+        "nivel": 3,
+        "código": "MAI",
+        "regularizadas": ["AM2"],
+        "aprobadas": ["AM1", "AGA"]
+      },
+      {
+        "nombre": "Mecánica de Fluidos",
+        "nivel": 3,
+        "código": "MdF",
+        "regularizadas": ["F1", "AM2"],
+        "aprobadas": ["AM1"]
+      },
+      {
+        "nombre": "Análisis Estructural II",
+        "nivel": 3,
+        "código": "AE2",
+        "regularizadas": ["AM2", "AE1"],
+        "aprobadas": ["AGA", "F1"]
+      },
+      {
+        "nombre": "Teoría del Buque II",
+        "nivel": 4,
+        "código": "TB2",
+        "regularizadas": ["IT1", "TB1", "MAI", "MdF"],
+        "aprobadas": ["AM2", "DN"]
+      },
+      {
+        "nombre": "Alistamiento de Buques",
+        "nivel": 4,
+        "código": "AB",
+        "regularizadas": ["AE2", "Term", "MdF"],
+        "aprobadas": ["DN"]
+      },
+      {
+        "nombre": "Máquinas Alternativas y Turbomáquinas",
+        "nivel": 4,
+        "código": "MAT",
+        "regularizadas": ["Term", "MR"]
+      },
+      {
+        "nombre": "Construcción Naval",
+        "nivel": 4,
+        "código": "CN",
+        "regularizadas": ["AE2", "TB1"],
+        "aprobadas": ["DN"]
+      },
+      {
+        "nombre": "Análisis Estructural III",
+        "nivel": 4,
+        "código": "AE3",
+        "regularizadas": ["AE2", "MAI"],
+        "aprobadas": ["AE1"]
+      },
+      {
+        "nombre": "Materiales Navales",
+        "nivel": 4,
+        "código": "MN",
+        "regularizadas": ["AE2"],
+        "aprobadas": ["QG"]
+      },
+      {
+        "nombre": "Inglés II",
+        "nivel": 4,
+        "código": "IT2",
+        "regularizadas": ["IT1"],
+      },
+      {
+        "nombre": "Actividad Naviera",
+        "nivel": 4,
+        "código": "AN",
+        "regularizadas": ["Leg"],
+      },
+      {
+        "nombre": "Proyecto de Buques",
+        "nivel": 5,
+        "código": "PB",
+        "regularizadas": ["TB2", "AB", "CN", "IT2"],
+        "aprobadas": ["IT1", "TB1"]
+      },
+      {
+        "nombre": "Plantas Eléctricas Navales",
+        "nivel": 5,
+        "código": "PEN",
+        "aprobadas": ["EME"]
+      },
+      {
+        "nombre": "Plantas Propulsoras Navales",
+        "nivel": 5,
+        "código": "PPN",
+        "regularizadas": ["AB", "MAT"],
+        "aprobadas": ["Term"]
+      },
+      {
+        "nombre": "Cálculo de Estructuras de Buques",
+        "nivel": 5,
+        "código": "CEB",
+        "regularizadas": ["AE3", "MN", "CN"],
+        "aprobadas": ["MAI", "AE2"]
+      },
+      {
+        "nombre": "Organización Industrial",
+        "nivel": 5,
+        "código": "OI",
+        "regularizadas": ["CN"],
+      },
+      {
+        "nombre": "Mecánica Aplicada a las Máquinas",
+        "nivel": 5,
+        "código": "MAM",
+        "regularizadas": ["AB", "AE3", "MN"],
+        "aprobadas": ["MR"]
+      },
+      {
+        "nombre": "Soldadura",
+        "nivel": 5,
+        "código": "Sold",
+        "regularizadas": ["MN"],
+        "aprobadas": ["MAI"]
+      },
+      {
+        "nombre": "Análisis por Elementos Finitos",
+        "nivel": 5,
+        "código": "AEF",
+        "regularizadas": ["AE3"],
+        "aprobadas": ["MAI"]
+      },
+      {
+        "nombre": "Proyecto Final",
+        "nivel": 6,
+        "código": "PF",
+        "regularizadas": ["PB", "PEN", "PPN", "CEB", "OI", "MAM", "Sold", "AEF"],
+        "aprobadas": ["TB2", "AB", "MAT", "CN", "AE3", "MN", "IT2", "AN"]
+      },
+      {
+        "nombre": "Seguridad, Higiene e Ingeniería Ambiental",
+        "nivel": 6,
+        "código": "SHIA",
+        "regularizadas": ["OI"],
+      },
+      {
+        "nombre": "Embarcaciones Veloces",
+        "nivel": 6,
+        "código": "EV",
+        "regularizadas": ["PB"],
+      },
+      {
+        "nombre": "Procedimientos de Astilleros",
+        "nivel": 6,
+        "código": "PA",
+        "regularizadas": ["PB", "CN", "OI"],
+      },
+      {
+        "nombre": "Práctica Profesional Supervisada",
+        "nivel": 6,
+        "código": "PPS",
+        "regularizadas": ["PB", "PEN", "PPN", "CEB", "OI", "MAM", "Sold", "AEF"],
+        "aprobadas": ["TB2", "AB", "MAT", "CN", "AE3", "MN", "IT2", "AN"]
+      },
     ]
 
   }
